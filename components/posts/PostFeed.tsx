09f979cf6c8bea1427ab266d40830ce4fd7f6c5a
fuadps <fuadpls@gmail.com>
2023-09-10 13:10:36 +0800

    view and create post

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,25 @@
+import usePosts from "@/hooks/usePosts";
+import PostItem from "@/components/posts/PostItem";
+
+interface PostFeedProps {
+    userId?: string
+
+}
+
+const PostFeed: React.FC<PostFeedProps> = ({userId}) => {
+    const {data: posts = []} = usePosts()
+
+    return (
+        <>
+            {posts.map((post: Record<string, any>) => (
+                <PostItem
+                    userId={userId}
+                    key={post.id}
+                    data={post}
+                />
+            ))}
+        </>
+    )
+}
+
+export default PostFeed
